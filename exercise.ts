f1d00ee15bf6c8dc248dd46619906e526ace112d
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-04-06 11:46:35 -0700

    teacher's anwser

--- exercise.ts
+++ exercise.ts
@@ -77,7 +77,7 @@ export default () => {
     const sequence: number[] = Array.from(Array(10).keys());
     const animals:string[] = ['pangolin', 'aardvark', 'echidna', 'binturong'];
     const stringsAndNumbers:(number|string)[] = [1, 'one', 2, 'two', 3, 'three'];
-    const allMyArrays: Array<number[]|string[]|(number|string)[]> = [sequence, animals, stringsAndNumbers];
+    const allMyArrays: Array<Array<number|string>> = [sequence, animals, stringsAndNumbers];
   
     console.log('Exercise 1.5', allMyArrays);
   
